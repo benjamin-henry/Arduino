9196fd9f154dc2839367a754295a5870848966b6
benjamin-henry <47706397+benjamin-henry@users.noreply.github.com>
2019-04-02 12:44:09 +0200

    Add files via upload

---
+++ Lib_TimerOne_V2/TimerOne_V2_example_2/TimerOne_V2_example_2.ino
@@ -0,0 +1,59 @@
+#include <TimerOne_V2.h>
+
+
+/////////in Object.h/////////
+class Object
+{
+  static Object *_instance;
+  public:
+    Object();
+    ~Object();  
+    void speak();
+    static void run();
+};
+////////////////////////////////////END
+
+
+/////////in Object.cpp/////////
+Object* Object::_instance;
+Object::Object(){_instance = this;} //ctor
+Object::~Object(){} // dtor
+
+void Object::run()
+{
+  _instance->speak();
+}
+
+void Object::speak()
+{
+  Serial.println("Object speaking!");
+}
+////////////////////////////////////END
+
+
+
+TimerOne_V2 Timer1; // on instancie le Timer
+
+int builtin_led = LED_BUILTIN;
+
+void task_0() {
+  digitalWrite(builtin_led,!digitalRead(builtin_led)); // quand cette routine est appellée, la led change d'état
+}
+
+Object *obj; // IMPORTANT : create a pointer to Object type
+
+void setup()
+{
+  Serial.begin(115200);
+  pinMode(builtin_led, OUTPUT);
+
+  obj = new Object();
+
+  Timer1.add_task(obj->run, FREQUENCY, 1);
+  Timer1.add_task(task_0, FREQUENCY, 1);
+  
+  Timer1.begin();
+}
+
+void loop()
+{}
